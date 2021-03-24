30391e8ceefdee3c1ae8d5a7b9c89d372755793e
Cristopher Saraos <cristopher.saraos@siigroup.cl>
2021-03-23 21:52:43 -0300

    add router and home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { BudgetComponent } from "./budget/budget.component";
+import { HomeComponent } from "./home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: "",
+		component: HomeComponent,
+	},
+    {
+		path: "budget",
+		component: BudgetComponent,
+	},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
